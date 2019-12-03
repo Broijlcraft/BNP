c4e2e1d2700c3a82df836b34957acd8e7ffed57d
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2019-12-03 08:32:44 +0100

    pointer

---
+++ Better Name Pending/Assets/Scripts/Keypad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    public Text keypadText;
+    public int valueToGive;
+    public int currentKeypadText;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Hand")
+        {
+            KeypadScreen(valueToGive);
+        }
+    }
+
+    public void KeypadScreen(int value)
+    {
+        print(currentKeypadText + "" + value);
+    }
+}
---
+++ Better Name Pending/Assets/Scripts/Pointer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public float defLength;
+    public GameObject dot;
+    public InputModule inputModule;
+
+    private LineRenderer lineRenderer;
+
+    public void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    private void UpdateLine()
+    {
+        float targetLength = defLength;
+
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        Vector3 endPos = transform.position + (transform.forward * targetLength);
+
+        if (hit.collider != null)
+        {
+            endPos = hit.point;
+        }
+
+        dot.transform.position = endPos;
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, endPos);
+    }
+
+    private RaycastHit CreateRaycast(float lenght)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, defLength);
+
+        return hit;
+    }
+}
