a8562cc135d229cbfd668d80fc4f9498c2733f3d
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-01-02 21:55:23 +0100

    blacklight controls

--- Better Name Pending/Assets/Scripts/Inheritance/TempLight.cs
+++ Better Name Pending/Assets/Scripts/Inheritance/TempLight.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class TempLight : Interactable {
 
-
+    public Light lamp;
+    public bool onOff; //hide
+    
     private void Start() {
         StartSetUp();
     }
 
-
     public override void Use(bool down) {
-        base.Use(down);
+        if (down) {
+            if (!hasBeenDown) {
+                if (onOff) {
+                    lamp.gameObject.SetActive(false);
+                    onOff = false;
+                } else {
+                    lamp.gameObject.SetActive(true);
+                    onOff = true;
+                }
+                hasBeenDown = true;
+            }
+        } else {
+            hasBeenDown = false;
+        }
     }
 
     public override void StartSetUp() {
