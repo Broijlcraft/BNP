07ee594e8470b748e6e22a1bb1403cb5f795ff28
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-04 17:19:18 +0100

    Niet werkende shader

---
+++ Better Name Pending/Assets/Scripts/BlackLightSource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BlackLightSource : MonoBehaviour
+{
+    public Material reveal;
+    public Light blackLight;
+    void Update()
+    {
+        reveal.SetVector("_LightPosition", blackLight.transform.position);
+        reveal.SetVector("_LightDirection", -blackLight.transform.forward);
+        reveal.SetFloat("_LightAngle", blackLight.spotAngle);
+    }
+}
