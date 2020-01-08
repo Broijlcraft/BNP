937b1612fd7e92636b237c4ffd5509db7ddcf094
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-08 12:48:09 +0100

    Renewed audio manager

--- Better Name Pending/Assets/Scripts/AudioManager.cs
+++ Better Name Pending/Assets/Scripts/AudioManager.cs
@@ -5,10 +5,11 @@ using UnityEngine.Audio;
 
 public static class AudioManager 
 {
-    public static void PlaySound(AudioClip audioClipToPlay)
+    public static void PlaySound(AudioClip audioClipToPlay, AudioMixer audioMixerToPlayFrom, string mixerGroup)
     {
         GameObject soundGameobject = new GameObject("Sound");
         AudioSource audioSource = soundGameobject.AddComponent<AudioSource>();
+        audioSource.outputAudioMixerGroup = audioMixerToPlayFrom.FindMatchingGroups(mixerGroup)[0];
         audioSource.PlayOneShot(audioClipToPlay);
         GameObject.Destroy(soundGameobject, audioClipToPlay.length);
     }
