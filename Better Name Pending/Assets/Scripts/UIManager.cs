6bffbbc072492ddbde8fb00dd9e28627c23f25fa
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-10 15:27:58 +0100

    ingame option panel

--- Better Name Pending/Assets/Scripts/UIManager.cs
+++ Better Name Pending/Assets/Scripts/UIManager.cs
@@ -7,15 +7,18 @@ public class UIManager : MonoBehaviour
 {
     public AudioClip buttonSound;
     private Scene mainLevel;
-    public GameObject startMenu, optionMenu;
+    private Scene mainMenu;
+    public GameObject startMenu, optionMenu, igMenu;
     public void Awake() 
     {
         mainLevel = SceneManager.GetSceneByName("Main Level");
+        mainMenu = SceneManager.GetSceneByName("Main Menu");
         startMenu.SetActive(true);
         optionMenu.SetActive(false);
     }
     public void StartButton()
     {
+        AudioManager.PlaySound(buttonSound, AudioManager.AudioGroups.UISFX);
         LoadNewLevel(mainLevel);
     }
     public void Options()
@@ -32,10 +35,21 @@ public class UIManager : MonoBehaviour
     }
     public void LoadNewLevel(Scene sceneToLoad)
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(sceneToLoad.ToString());
     }
     public void Quit()
     {
         Application.Quit();
+    }
+    public void OptionIG()
+    {
+        AudioManager.PlaySound(buttonSound, AudioManager.AudioGroups.UISFX);
+        igMenu.SetActive(false);
+        optionMenu.SetActive(true);
+    }
+    public void BackToMainMenu()
+    {
+        AudioManager.PlaySound(buttonSound, AudioManager.AudioGroups.UISFX);
+        LoadNewLevel(mainMenu);   
     }
 }
