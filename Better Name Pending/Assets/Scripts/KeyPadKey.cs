430fdc040c74f25e4bf8d666c6375f62ceebb24b
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-01-10 21:54:17 +0100

    keypad

--- Better Name Pending/Assets/Scripts/KeyPadKey.cs
+++ Better Name Pending/Assets/Scripts/KeyPadKey.cs
@@ -1,38 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class KeyPadKey : MonoBehaviour {
 
+    public Text uiValue;
     public int value;
     public Transform origin;
     public float range;
+    public string fingerTag;
     Keypad keyPad;
+    public bool fingerInRange;
     [HideInInspector] public Material material;
 
     private void Start() {
         keyPad = GetComponentInParent<Keypad>();
         material = GetComponent<Renderer>().material;
+        ChangeColor(keyPad.keyColor);
+        uiValue.color = keyPad.keyColor;
     }
 
     private void Update() {
-        if (Input.GetButtonDown("Jump")) {
-            if (material) {
-                //material.EnableKeyword("_EmissionColor");
-                print("Mat");
-                material.SetColor("_EmissionColor", keyPad.keyColor);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, range);
+        for (int i = 0; i < colliders.Length; i++) {
+            if (Array.Exists(colliders, element => element.transform.tag == fingerTag)) {
+                if (!fingerInRange) {
+                    ChangeColor(keyPad.pressedKeyColor);
+                    uiValue.color = keyPad.pressedKeyColor;
+                    AudioManager.PlaySound(keyPad.keyPress, AudioManager.AudioGroups.GameSFX);
+                    keyPad.AddNumber(value);
+                    fingerInRange = true;
+                }
             } else {
-                print("No Mat");
+                uiValue.color = keyPad.keyColor;
+                ChangeColor(keyPad.keyColor);
+                fingerInRange = false;
             }
         }
     }
+
+    void ChangeColor(Color color) {
+        material.SetColor("_EmissionColor", color);
+    }
+
     private void OnDrawGizmos() {
         if (keyPad && keyPad.showGismoz) {
-            if (origin) {
-                Gizmos.DrawWireSphere(origin.position, range);
-            } else {
-                Gizmos.DrawWireSphere(transform.position, range);
-            }
+            Gizmos.DrawWireSphere(transform.position, range);
         }        
     }
 }
--- Better Name Pending/Assets/Scripts/Keypad.cs
+++ Better Name Pending/Assets/Scripts/Keypad.cs
@@ -6,14 +6,42 @@ using UnityEngine;
 public class Keypad : MonoBehaviour {
     public Color keyColor;
     public Color pressedKeyColor;
+    public float resetDelay;
+    public AudioClip keyPress;
+    public AudioClip wrongCode;
+    public AudioClip unlockDoor;
     public bool showGismoz;
-    public int password;
-    public int currentValue;
+    public int passcode;
     public Text display;
+    int currentValue;
+    bool cooldown;
+
+    private void Start() {
+        ResetValues();
+    }
 
     public void AddNumber(int number) {
-        display.text = display.text + number;
-        currentValue +
+        if(currentValue < 999 && !cooldown) {
+            display.color = keyColor;
+            currentValue = currentValue * 10;
+            currentValue += number;
+            display.text = currentValue.ToString();
+            if(currentValue > 999) {
+                cooldown = true;
+                if (currentValue == passcode) {
+                    AudioManager.PlaySound(unlockDoor, AudioManager.AudioGroups.GameSFX);
+                } else {
+                    Invoke("ResetValues", resetDelay);
+                    display.color = pressedKeyColor;
+                    AudioManager.PlaySound(wrongCode, AudioManager.AudioGroups.GameSFX);
+                }
+            }
+        }
+    }
+
+    void ResetValues() {
+        cooldown = false;
+        currentValue = 0;
+        display.text = "";
     }
 }
-
