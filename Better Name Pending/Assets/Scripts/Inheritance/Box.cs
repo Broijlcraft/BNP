f38ca2bc66e7188767dd9613437e11a58dfa7ce3
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-10 11:18:40 +0100

    monkaS crowbar

---
+++ Better Name Pending/Assets/Scripts/Inheritance/Box.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public AudioClip audioClip;
+    public AudioManager.AudioGroups audioGroups;
+    public void BoxBreak()
+    {
+
+        AudioManager.PlaySound(audioClip, audioGroups);
+        gameObject.GetComponent<Animation>().Play();
+    }
+}
--- Better Name Pending/Assets/Scripts/Inheritance/Crowbar.cs
+++ Better Name Pending/Assets/Scripts/Inheritance/Crowbar.cs
@@ -2,32 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crowbar : InteractableItems
+public class Crowbar : Interactable
 {
-    private Animation boxAnim;
-
-    public void OnCollisionEnter(Collision collision)
+    public float minimumVelocity;
+    public Rigidbody rb;
+    public Transform[] crowbarParts;
+    public void Update() 
     {
-        if (collision.gameObject.tag == "Box")
+        if(rb.velocity.magnitude >= minimumVelocity)
         {
-            boxAnim = collision.gameObject.GetComponent<Animation>();
-            BreakBox();
-            BreakItem();
-        }    
-    }
-
-    private void BreakBox()
-    {
-        boxAnim.Play();
-    }
-
-    private void BreakItem()
-    {
-        AudioManager.PlaySound(audioClip, AudioManager.AudioGroups.GameSFX);
-        Destroy(gameObject, 0.1f);
-        for (int i = 0; i < brokenItems.Length; i++)
-        {
-            Instantiate(brokenItems[i], transform.position, transform.rotation);
+            Collider[] colliders = Physics.OverlapSphere(origin.position, range);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].CompareTag("Box"))
+                {
+                    colliders[i].GetComponent<Box>().BoxBreak();
+                    CrowbarBreak();
+                    break;
+                }
+            }
         }
+    }
+    private void CrowbarBreak()
+    {
+        for (int i = 0; i < crowbarParts.Length; i++)
+        {
+            Rigidbody rigid2 = crowbarParts[i].GetComponent<Rigidbody>();
+            if (rigid2)
+            {
+                rigid2.isKinematic = false;
+                rigid2.useGravity = true;
+            }
+            crowbarParts[i].SetParent(null);
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos() 
+    {
+        ShowGizmos(origin, range);
     }
 }
