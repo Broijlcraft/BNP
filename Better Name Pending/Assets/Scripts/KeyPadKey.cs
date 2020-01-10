ea9009a8f8e8fde7f1d54b5394645b8d52299457
Broijlcraft <49680186+Broijlcraft@users.noreply.github.com>
2020-01-10 18:08:34 +0100

    push

--- Better Name Pending/Assets/Scripts/KeyPadKey.cs
+++ Better Name Pending/Assets/Scripts/KeyPadKey.cs
@@ -5,5 +5,34 @@ using UnityEngine;
 public class KeyPadKey : MonoBehaviour {
 
     public int value;
+    public Transform origin;
+    public float range;
+    Keypad keyPad;
+    [HideInInspector] public Material material;
 
+    private void Start() {
+        keyPad = GetComponentInParent<Keypad>();
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            if (material) {
+                //material.EnableKeyword("_EmissionColor");
+                print("Mat");
+                material.SetColor("_EmissionColor", keyPad.keyColor);
+            } else {
+                print("No Mat");
+            }
+        }
+    }
+    private void OnDrawGizmos() {
+        if (keyPad && keyPad.showGismoz) {
+            if (origin) {
+                Gizmos.DrawWireSphere(origin.position, range);
+            } else {
+                Gizmos.DrawWireSphere(transform.position, range);
+            }
+        }        
+    }
 }
--- Better Name Pending/Assets/Scripts/Keypad.cs
+++ Better Name Pending/Assets/Scripts/Keypad.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class Keypad : MonoBehaviour
-{
-    public static string codePassword;
-    public static int currentValue;
+public class Keypad : MonoBehaviour {
+    public Color keyColor;
+    public Color pressedKeyColor;
+    public bool showGismoz;
+    public int password;
+    public int currentValue;
+    public Text display;
 
-    public void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.tag == "Hand")
-        {
-            currentValue = currentValue + '1';
-            
-        }
+    public void AddNumber(int number) {
+        display.text = display.text + number;
+        currentValue +
     }
 }
+
