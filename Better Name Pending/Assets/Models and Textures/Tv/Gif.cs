5f32bfb4557da0093bc0f6e79e3d4d4f6ea466ee
Johannes <johanspeek@gmail.com>
2020-01-16 20:56:50 +0100

    credits fix, dropdown options, paintings and gifs, + voicelines imports

---
+++ Better Name Pending/Assets/Models and Textures/Tv/Gif.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gif : MonoBehaviour
+{
+    public Texture[] frames;
+    public int currentGif;
+    public float refreshRate;
+    public Material renderTexture;
+    public Material materiall;
+    public void Start()
+    {
+        materiall = GetComponent<Renderer>().material;
+        InvokeRepeating("FrameUpdate", 0, refreshRate);
+    }
+    public void FrameUpdate()
+    {
+        print(currentGif);
+        if (frames.Length > 0)
+        {
+            if (currentGif < frames.Length-1)
+            {
+                currentGif++;
+            }
+            else
+            {
+                currentGif = 0;
+            }
+            materiall.SetTexture("_BaseMap", frames[currentGif]);
+            //renderTexture.mainTexture = frames[currentGif];
+        } 
+    }
+}
---
+++ Better Name Pending/Assets/UI/ArrayGif.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrayGif : MonoBehaviour
+{
+    public Texture2D[] frames;
+    public int framesPerSecond;
+
+    public void Update()
+    {
+        int index = (int) Time.time * framesPerSecond;
+        index = index % frames.Length;
+        GetComponent<Material>().mainTexture = frames[index];
+    }
+}
