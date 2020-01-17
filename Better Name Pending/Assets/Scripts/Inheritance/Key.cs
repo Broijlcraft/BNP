33a29b1d96f52b3dcc0bf896087593497fb80031
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2020-01-17 10:43:01 +0100

    key functionality

--- Better Name Pending/Assets/Scripts/Inheritance/Key.cs
+++ Better Name Pending/Assets/Scripts/Inheritance/Key.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class Key : InteractableItems
 {
-    public Transform keyHole;
-    public float minDistance;
-    public bool hasInserted;
-
-    private void Update()
+    public void OnCollisionEnter(Collision other) 
     {
-        float distance = Vector3.Distance(keyHole.position, transform.position);
-
-        if (hasInserted)
+        if(other.transform.tag == "Slot")
         {
-            if (keyHole.GetComponentInParent<KeyHoleInv>().doorAnim.isPlaying == true)
-            {
-                transform.position = keyHole.position;
-                transform.LookAt(keyHole);
-            }
-        }
-
-        else if (distance <= minDistance)
-        {
-            hasInserted = true;
-            keyHole.GetComponentInParent<KeyHoleInv>().UnlockDoor(gameObject);
+            gameObject.GetComponentInChildren<FollowObject>().freezeFollow = false;
         }
     }
 }
