6ada7051611f954b93d0d08a368d175fc71484e4
kubilay0 <49680603+2kubilay0@users.noreply.github.com>
2020-01-17 13:47:45 +0100

    Revert "script gefixet weer"
    This reverts commit 99e551be232dea9677810415feeaadcfc492e813.

--- Better Name Pending/Assets/Scripts/Inheritance/Key.cs
+++ Better Name Pending/Assets/Scripts/Inheritance/Key.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class Key : InteractableItems
 {
-    public Component[] followObject;
-    public void OnTriggerEnter(Collider other) {
+    public void OnCollisionEnter(Collision other) 
+    {
         if(other.transform.tag == "Slot")
         {
-            followObject = other.gameObject.GetComponentsInChildren<FollowObject>();
-
-            foreach (FollowObject scripts in followObject)
-            {
-                scripts.freezeFollow = false;
-            }
-            other.gameObject.GetComponentInParent<Rigidbody>().useGravity = true;
-            other.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+            gameObject.GetComponentInChildren<FollowObject>().freezeFollow = false;
         }
     }
 }
